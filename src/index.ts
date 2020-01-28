3e14e5c0ba6af22d9b59cbdc4220c759b303e655
William L <w-lindvall@outlook.com>
2020-01-28 13:58:45 -0800

    accept transition opts as param

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,18 @@
-interface IRetVal {
-  measure: TMeasure,
-  animate: TAnimate,
-}
-type TMeasure = () => IRetVal
-type TAnimate = (
-  stylingFn: () => any,
-  newEl?: HTMLElement
-) => IRetVal
-
 const init = (el: HTMLElement): IRetVal => ({
   measure: () => measure(el),
-  animate: (stylingFn, newEl) => animate(
-    newEl || el,
+  animate: (stylingFn, animationOpts) => animate(
+    el,
     el.getBoundingClientRect(),
     stylingFn,
+    animationOpts,
   ),
 })
 
-const animate = (
+const transform = (
   el: HTMLElement,
   initialBounds: ClientRect,
-  stylingFn: () => any,
-) : IRetVal => {
-  stylingFn() // layout change
-
+  options?: IAnimationOpts,
+): ClientRect => {
   const finalBounds: ClientRect = el.getBoundingClientRect()
   const deltaX: number = initialBounds.left - finalBounds.left
   const deltaY: number = initialBounds.top - finalBounds.top
@@ -31,38 +20,82 @@ const animate = (
   const deltaH: number = initialBounds.height / finalBounds.height
 
   el.animate([{
-    transformOrigin: 'top left', // TODO: passed as param?
+    transformOrigin: (
+      options && options.transformOrigin
+    ) || 'top left',
     transform: `
       translate(${deltaX}px, ${deltaY}px)
       scale(${deltaW}, ${deltaH})
     `,
   }, {
-    transformOrigin: 'top left',
+    transformOrigin: (
+      options && options.transformOrigin
+    ) || 'top left',
     transform: 'none',
   }], {
-    duration: 300, // TODO: allow to be passed as param
-    easing: 'ease-in-out',
+    duration: (options && options.duration) || 300,
+    easing: (options && options.ease) || 'ease-in-out',
     fill: 'both',
   })
 
+  return finalBounds
+}
+
+const animate = (
+  el: HTMLElement,
+  initialBounds: ClientRect,
+  stylingFn: () => any,
+  animationOpts?: IAnimationOpts,
+) : IRetVal => {
+  stylingFn() // layout change
+
+  const finalEl = (animationOpts && animationOpts.to) || el
+  const finalBounds = transform(
+    finalEl,
+    initialBounds,
+    animationOpts,
+  )
+
   return ({
     measure: () => measure(el),
-    animate: (stylingFn, newEl) => animate(
-      newEl || el,
+    animate: (stylingFn, animationOpts) => animate(
+      el,
       finalBounds,
-      stylingFn
+      stylingFn,
+      animationOpts,
     ),
   })
 }
 
 const measure = (el: HTMLElement): IRetVal => ({
   measure: () => measure(el),
-  animate: (stylingFn, newEl) => animate(
-    newEl || el,
+  animate: (stylingFn, animationOpts) => animate(
+    el,
     el.getBoundingClientRect(),
     stylingFn,
+    animationOpts,
   )
 })
 
+interface IRetVal {
+  measure: TMeasure,
+  animate: TAnimate,
+}
+
+type TMeasure = () => IRetVal
+
+type TAnimate = (
+  stylingFn: () => any,
+  animationOpts?: IAnimationOpts,
+) => IRetVal
+
+interface IAnimationOpts {
+  to?: HTMLElement,
+  duration?: number,
+  transformOrigin?: string,
+  ease?: string,
+}
+
 export {init}
 export default {init}
+
